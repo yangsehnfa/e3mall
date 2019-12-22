95faa3e6aaf6464e7c7832634abc3c4aa814cc78
fage <756507254@qq.com>
2019-12-22 15:26:08 +0800

    提交页面信息

---
+++ e3mall-parent/e3mall-manager/e3mall-manager-web/src/main/java/cn/e3mall/manager/controller/PageController.java
@@ -0,0 +1,13 @@
+package cn.e3mall.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("{page}")
+    public  String showPage(@PathVariable("page") String page){
+        return page;
+    }
+}
